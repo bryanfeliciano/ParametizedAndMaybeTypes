7a6117ee5f3d6210d71bb27f4763a5f8218d0635
bryan <bryanfeliciano892@gmail.com>
2022-04-24 11:38:26 -0500

    initial commit

---
+++ parametized.hs
@@ -0,0 +1,65 @@
+import qualified Data.Map as Map
+
+data Organ = Heart | Brain | Kidney | Spleen deriving (Show , Eq)
+
+organs:: [Organ]
+organs = [Heart,Heart,Brain,Spleen,Spleen,Kidney]
+
+ids:: [Int]
+ids = [2,7,13,14,21,24]
+
+-- Organ pairs created by using zip --
+
+organPairs :: [(Int,Organ)]
+organPairs = zip ids organs
+
+-- Put together an organ catalog --
+
+organCatalog :: Map.Map Int Organ
+organCatalog = Map.fromList organPairs
+
+-- Now you can look up by using Map.lookup index organCatalog --
+
+data Triple a = Triple a a a deriving (Show)
+
+type Point3D = Triple Double
+
+aPoint :: Point3D
+aPoint = Triple 0.1 53.2 12.3
+
+
+type FullName = Triple String
+type Initials = Triple Char
+
+aPerson :: FullName
+aPerson = Triple "Howard" "Phillips" "LoveCraft"
+
+initials :: Initials
+initials = Triple 'H' 'P' 'L'
+
+first :: Triple a -> a
+first (Triple x _ _) = x
+
+second :: Triple a -> a
+second (Triple _ x _) = x
+
+third :: Triple a -> a 
+third (Triple _ _ x) = x
+
+data Box a = Box a deriving Show
+
+wrap:: a -> Box
+wrap x = Box x
+
+unWrap :: Box -> x
+unWrap (Box x) = x
+
+-- Chapter objective functions --
+
+-- Q18.1 --
+
+boxMap :: ( a -> b) -> Box a -> Box b
+boxMap func (Box val) = Box (func val)
+
+tripleMap :: (a -> b) -> Triple a -> Triple b
+tripleMap func (Triple v1 v2 v3) = Triple (func v1) (func v2) (func v3)
