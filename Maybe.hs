878f213f3e121cd62feee7cae3595ead29bf3ea2
bryan <bryanfeliciano892@gmail.com>
2022-04-24 14:07:58 -0500

    container implemented and display function written

--- Maybe.hs
+++ Maybe.hs
@@ -2,6 +2,8 @@ import qualified Data.Map as Map
 
 data Organ = Heart | Brain | Kidney | Spleen deriving (Show , Eq , Ord , Enum)
 
+data Container = Vat Organ | Cooler Organ | Bag Organ
+
 allOrgans:: [Organ]
 allOrgans = [Heart ..Kidney]
 
@@ -35,3 +37,16 @@ isSomething :: Maybe Organ -> Bool
 isSomething Nothing = False
 isSomething (Just _) = True
 
+justTheOrgans :: [Maybe Organ]
+justTheOrgans = filter isSomething availableOrgans
+
+showOrgan :: Maybe Organ -> String
+showOrgan (Just organ) = show organ
+
+organList :: [String]
+organList = map showOrgan justTheOrgans
+
+instance Show Container where
+    show (Vat organ) = show organ ++ "In a vat"
+    show (Cooler organ) = show organ ++ "In a cooler"
+    show (Bag organ) = show organ ++ "In a bag"
